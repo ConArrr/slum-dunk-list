73c8ef529822e56a900362116cb8c3167158f8d6
ConArrr <conarrr7714@gmail.com>
2020-12-19 15:21:07 +0900

    Home 画面実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 // eslint-disable-next-line no-use-before-define
 import React, { FC } from 'react'
+import { Switch, Route } from 'react-router'
 
-const App: FC = () => <div className="container" />
+import Home from 'components/pages/Home'
+
+const App: FC = () => (
+  <div className="container">
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </div>
+)
 
 export default App

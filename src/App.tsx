6bd8551ea4ab199773d30baa3d179e56f444d9bb
ConArrr <conarrr7714@gmail.com>
2020-12-19 14:57:02 +0900

    Data用意。App.tsx, index.tsx の土台づくり。

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-use-before-define
+import React, { FC } from 'react'
+
+const App: FC = () => {
+  return <div className="container" />
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './App'
+import 'semantic-ui-css/semantic.min.css'
+import './index.css'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+)
